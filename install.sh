ddea529017a004a177b4000b66a4d79ba26107ea
Paramvir Sran <paramvirsran@gmail.com>
2024-07-25 13:03:15 -0400

    Updated submodules to their latest commits

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-stow zshrc tmux nvim alacritty git kitty
+
+# Update all submodules to the latest commit on their current branch
+git submodule update --remote --merge
+
+# Add changes to the main repository
+git add .
+
+# Commit changes with a generic message
+git commit -m "Updated submodules to their latest commits"
+
+# Push changes to the remote repository
+git push
+
+# Unstow and restow each package
+stow -D */ # Unstow all packages
+stow */   # Restow all packages
+
+echo "All submodules have been updated and symlinks refreshed."
